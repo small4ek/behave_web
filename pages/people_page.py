86ea8c4c7e63cb21488616fe0819348fa26d778c
Oleksii Danylyuk <danilyuk@bigmir.net>
2018-02-01 09:12:50 +0200

    compare list smooth

--- pages/people_page.py
+++ pages/people_page.py
@@ -10,28 +10,30 @@ class PeoplePage(Page):
     """
 
     url = '/people'
-
+    list_name = []
+    letter_list = []
+    rletter = ' '
 
     def create_list_name(self):
+        global list_name
         list_name = []
         for i in self.context.driver.find_elements_by_css_selector('a.name'):
-                list_name.append(i.text)
-                #print(list_name)
+            list_name.append(i.text)
+        return list_name
 
     def choose_letter(self):
         alphabet = []
+        global letter_list
+        global rletter
         for i in self.context.driver.find_elements_by_css_selector('li>a.pagination-item'):
             alphabet.append(i.text)
         rletter = alphabet[random.randint(0, len(alphabet) - 1)]
         xpath = "//a[@class='pagination-item'][text()='" + rletter + "']"
         self.context.driver.find_element_by_xpath(xpath).click()
         letter_list = self.create_list_name()
-        #print(letter_list)
-
 
     def compare_list(self):
-        #pass
-        if any(item.startswith(self.choose_letter().rletter) for item in self.create_list_name().list_name) == self.choose_letter().letter_list:
+        if any(item.startswith(self.rletter) for item in self.list_name) == (self.letter_list):
             return 1
         else:
             return 0
