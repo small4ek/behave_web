c74d973ca2d63c44e4691dae2acbd94405e1a145
Oleksii Danylyuk <danilyuk@bigmir.net>
2018-02-06 13:24:06 +0200

    without global

--- features/steps/step_selenium_filter_people_alphabet.py
+++ features/steps/step_selenium_filter_people_alphabet.py
@@ -26,11 +26,11 @@ def step_impl(context):
 @given('we are on People Page')
 def step_impl(context):
     context.people_page.navigate()
+    context.people_page.set_list_name()
 
 
 @when('we choose a letter')
 def step_impl(context):
-    context.people_page.create_list_name()
     context.people_page.create_alphabet()
     context.people_page.choose_letter()
 
--- pages/people_page.py
+++ pages/people_page.py
@@ -11,32 +11,31 @@ class PeoplePage(Page):
 
     url = '/people'
     list_name = []
-    letter_list = []
+    rletter = ''
 
     def create_list_name(self):
-        global list_name
         list_name = []
         for i in self.context.driver.find_elements_by_css_selector('a.name'):
             list_name.append(i.text)
         return list_name
 
+    def set_list_name(self):
+        self.list_name = self.create_list_name()
+
     def create_alphabet(self):
-        global alphabet
         alphabet = []
         for i in self.context.driver.find_elements_by_css_selector('li>a.pagination-item'):
             alphabet.append(i.text)
         return alphabet
 
     def choose_letter(self):
-        global letter_list
-        global rletter
-        rletter = self.create_alphabet()[random.randint(0, len(self.create_alphabet()) - 1)]
-        xpath = "//a[@class='pagination-item'][text()='" + rletter + "']"
+        self.rletter = self.create_alphabet()[random.randint(0, len(self.create_alphabet()) - 1)]
+        xpath = "//a[@class='pagination-item'][text()='" + self.rletter + "']"
         self.context.driver.find_element_by_xpath(xpath).click()
-        letter_list = self.create_list_name()
 
     def compare_list(self):
-        if any(item.startswith(self.rletter) for item in self.list_name) == (self.letter_list):
+        letter_list = self.create_list_name()
+        if any(item.startswith(self.rletter) for item in self.list_name) == letter_list:
             return 1
         else:
             return 0
