2168213817537222527d45e1672e71a9e5ca4363
genasmetchik@gmail.com <genasmetchik@gmail.com>
2018-02-14 15:04:01 +0200

    remove pass

--- features/steps/step_invite_your_team.py
+++ features/steps/step_invite_your_team.py
@@ -3,32 +3,29 @@ from behave import when, then, given
 
 @given('we are on Lobby Page')
 def step_impl(context):
-    # context.lobby_page.navigate()
-    pass
+    context.lobby_page.navigate()
+
 
 @when('we click Invite your team')
 def step_impl(context):
-    # context.lobby_page.invite_team_form()
-    pass
+    context.lobby_page.invite_team_form()
+
 
 @when('we add the emails')
 def step_impl(context):
-    # context.lobby_page.invite_team_email_input()
-    pass
+    context.lobby_page.invite_team_email_input()
+
 
 @when('we delete one email')
 def step_impl(context):
-    # context.lobby_page.delete_email_from_list()
-    pass
+    context.lobby_page.delete_email_from_list()
 
 
 @when('we send invite')
 def step_impl(context):
-    # context.lobby_page.click_send_invite()
-    pass
+    context.lobby_page.click_send_invite()
 
 
 @then('we see success message')
 def step_impl(context):
-    # assert 'Invites sent' in context.lobby_page.success_invite_message()
-    pass
+    assert 'Invites sent' in context.lobby_page.success_invite_message()
