516a7cd2c7fc3f48479f3c539f03d3cf49fad902
genasmetchik@gmail.com <genasmetchik@gmail.com>
2018-02-15 02:30:53 +0200

    add clear message in alias feature

--- pages/lobby_page.py
+++ pages/lobby_page.py
@@ -21,7 +21,7 @@ class LobbyPage(Page):
     def find_msg_field(self):
         return self.context.driver.find_element_by_id('hc-message-input')
 
-    def clear_all_message(self):
+    def clear_all_messages(self):
         self.context.wait.until(lambda driver: driver.find_element_by_id('hc-message-input'))
         LobbyPage.find_msg_field(self).send_keys('/clear')
         LobbyPage.find_msg_field(self).send_keys(Keys.RETURN)
@@ -29,7 +29,7 @@ class LobbyPage(Page):
     def send_msg_in_room(self, msg):
         if "room" in self.context.driver.current_url:
             if msg == '/clear':
-                self.clear_all_message()
+                self.clear_all_messages()
                 self.context.wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, 'hc-chat-msg')))
             else:
                 self.context.wait.until(lambda driver: driver.find_element_by_id('hc-message-input'))
@@ -146,8 +146,7 @@ class LobbyPage(Page):
         self.context.wait.until(EC.presence_of_element_located((By.ID, 'hc-message-input')))
         self.context.wait.until(EC.visibility_of_element_located(
             (By.CSS_SELECTOR, ".hc-chat-row.hc-msg-nocolor.hc-msg-message.hc-classic-neue")))
-        self.find_input_field().send_keys('/clear')
-        self.find_input_field().send_keys(Keys.ENTER)
+        self.clear_all_messages()
         alias_set_string = '/alias set ' + self.context.test_name + ' @TestHenaYamkoviy'
         self.find_input_field().send_keys(alias_set_string)
         self.find_input_field().send_keys(Keys.ENTER)
